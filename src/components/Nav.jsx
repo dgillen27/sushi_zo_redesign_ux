3db2113893e2a50b03ca373d1ace5c7f2f884c22
Daniel Gillen <danielgillen@Daniels-MBP.home>
2019-05-19 15:28:45 -0400

    fixed book in nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = (props) => {
         <img onClick={props.scrollHeader} src="https://s3.amazonaws.com/image-urls/images/sushizo-1.png" alt="" />
         <h2 onClick={props.scrollLocations}>LOCATIONS</h2>
         <h2 onClick={props.scrollPress}>PRESS</h2>
-        <div className="button" id="nav-book" onClick={props.scrollBook}>BOOK</div>
+        <div className="button" id="nav-book" onClick={props.scrollLocations}>BOOK</div>
       </div>
       <HamburgerMenu {...props}/>
       <img id="mobile-img" onClick={props.scrollHeader}  src="https://s3.amazonaws.com/image-urls/images/sushizo-1.png" alt="" />
